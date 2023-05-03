747a910cc970e5084e7020805b641459b240f8ee
AnnWolkova <volkov.nikita.a@gmai.com>
2023-05-03 10:12:49 +0700

    After JoCoCo testing2

--- src/main/java/ru/netology/qamvn/Radio.java
+++ src/main/java/ru/netology/qamvn/Radio.java
@@ -56,14 +56,8 @@ public class Radio {
     }
 
     public void lowerVolume() {
-        if (currentVolume !=0) {
+        if (currentVolume != 0) {
             currentVolume = currentVolume - 1;
-   //     if (currentVolume <= 100 && currentVolume > 0) {
-   //         currentVolume = currentVolume - 1;
-
-//        else if (currentVolume <= 0){
-//            currentVolume = currentVolume;
-
         }
     }
 }
